e94d361306db52c199156035ba94035ed2d3ffc5
gedom2012 <gedom2012@gmail.com>
2021-11-28 11:18:15 +0000

    CRUD finalized
    - GET by id
    - POST
    - PUT
    - DELETE

--- backend/src/main/java/com/mariath/crud/resource/ClientResource.java
+++ backend/src/main/java/com/mariath/crud/resource/ClientResource.java
@@ -1,14 +1,22 @@
 package com.mariath.crud.resource;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mariath.crud.DTO.ClientDTO;
 import com.mariath.crud.services.ClientService;
@@ -29,6 +37,33 @@ public class ClientResource {
 		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		Page<ClientDTO> list = service.findAllPaged(request);
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO client = service.findById(id);
+		return ResponseEntity.ok().body(client);
+	}
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		ClientDTO client = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(client);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO clientDto) {
+		ClientDTO client = service.update(id, clientDto);
+		return ResponseEntity.ok().body(client);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/com/mariath/crud/services/ClientService.java
+++ backend/src/main/java/com/mariath/crud/services/ClientService.java
@@ -20,6 +20,43 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest request) {
 		Page<Client> entity = repository.findAll(request);
 		return entity.map(x -> new ClientDTO(x));
-	}	
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Client entity = repository.getOne(id);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyToDtoEntity(dto, entity);
+		repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO clientDto) {
+		Client entity = repository.getOne(id);
+		copyToDtoEntity(clientDto, entity);
+		repository.save(entity);
+		return new ClientDTO(entity);
+
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		Client entity = repository.getOne(id);
+		repository.delete(entity);
+	}
+
+	private void copyToDtoEntity(ClientDTO clientDto, Client entity) {
+		entity.setName(clientDto.getName());
+		entity.setCpf(clientDto.getCpf());
+		entity.setBirthDate(clientDto.getBirthDate());
+		entity.setIncome(clientDto.getIncome());
+		entity.setChildren(clientDto.getChildren());
+	}
 
 }
